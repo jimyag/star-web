992aa74e777e3713e12e9ce5d7cbf02e32780aa3
jimyag <jimyag@126.com>
2021-09-12 14:00:00 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { Button, Form,Input,Icon ,message } from 'ant-design-vue';
+import './assets/css/style.css'
+const app = createApp(App);
+app.use(Input)
+app.use(Icon)
+app.use(router)
+app.use(Form)
+app.use(Button).mount('#app')
+app.config.globalProperties.$message=message
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Admin from '../views/Admin.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/admin',
+    name: 'admin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component:Admin,
   }
 ]
 
